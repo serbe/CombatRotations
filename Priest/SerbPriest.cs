da909a74fd14b8f17a72c2d3a35b3da1d55e8cea
serbe <mkoctuk@gmail.com>
2015-04-09 11:41:20 +0300

    Priest
    Add Priest Discipline

---
+++ Priest/SerbPriest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReBot
+{
+	public class SerbPriest
+	{
+		public SerbPriest ()
+		{
+		}
+	}
+}
+
---
+++ Priest/SerbPriestDiscipline.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ReBot.API;
+
+namespace ReBot
+{
+	[Rotation ("Serb Priest Discipline SC", "ReBot", WoWClass.Priest, Specialization.PriestDiscipline, 40, 25)]
+
+	public class SerbPriestDisciplineSC : SerbPriest
+	{
+		public SerbPriestDisciplineSC ()
+		{
+		}
+	}
+}
+
