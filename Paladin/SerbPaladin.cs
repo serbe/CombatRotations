fdf92f3d580bff94609348e6d0e87d19233f810b
serbe <mkoctuk@gmail.com>
2015-05-02 10:39:03 +0300

    Pal Prot

---
+++ Paladin/SerbPaladin.cs
@@ -0,0 +1,170 @@
+﻿using ReBot.API;
+using Newtonsoft.Json;
+using System;
+
+namespace ReBot
+{
+	public abstract class SerbPaladin : CombatRotation
+	{
+		[JsonProperty ("Use GCD")]
+		public bool Gcd = true;
+
+		// Consts && Vars
+
+		public bool InCombat;
+		public DateTime StartBattle;
+		public UnitObject CycleTarget;
+		public int BossHealthPercentage = 500;
+		public int BossLevelIncrease = 5;
+		public float OraliusWhisperingCrystalId = 118922;
+		public float CrystalOfInsanityId = 86569;
+
+		// Get
+
+		public int HolyPower {
+			get {
+				return Me.GetPower (WoWPowerType.PaladinHolyPower);
+			}
+		}
+
+		public float Range (UnitObject u = null)
+		{
+			u = u ?? Target;
+			return u.CombatRange;
+		}
+
+		public double Cooldown (string s)
+		{
+			return SpellCooldown (s) > 0 ? SpellCooldown (s) : 0;
+		}
+
+		public int EnemyInRange (int range)
+		{
+			int x = 0;
+			foreach (UnitObject mob in API.CollectUnits(range)) {
+				if ((mob.IsEnemy || Me.Target == mob) && !mob.IsDead && mob.IsAttackable) {
+					x++;
+				}
+			}
+			return x;
+		}
+
+		public double Time {
+			get {
+				TimeSpan combatTime = DateTime.Now.Subtract (StartBattle);
+				return combatTime.TotalSeconds;
+			}
+		}
+
+		public double Health (UnitObject u = null)
+		{
+			u = u ?? Target;
+			return u.HealthFraction;
+		}
+
+		// Check
+
+		public bool IsBoss (UnitObject u = null)
+		{
+			u = u ?? Target;
+			return(u.MaxHealth >= Me.MaxHealth * (BossHealthPercentage / 100f)) || u.Level >= Me.Level + BossLevelIncrease;
+		}
+
+		public bool IsPlayer (UnitObject u = null)
+		{
+			u = u ?? Target;
+			return u.IsPlayer;
+		}
+
+		public bool IsElite (UnitObject u = null)
+		{
+			u = u ?? Target;
+			return u.IsElite ();
+		}
+
+		public bool Usable (string s)
+		{
+			return HasSpell (s) && Cooldown (s) == 0;
+		}
+
+		public double DamageTaken (float t)
+		{
+			var damage = API.ExecuteLua<double> ("local ResolveName = GetSpellInfo(158300);local n,_,_,_,_,dur,expi,_,_,_,id,_,_,_,val1,val2,val3 = UnitAura(\"player\", ResolveName, nil, \"HELPFUL\");return val2");
+			if (Time < 10) {
+				if (Time < t / 1000)
+					return damage;
+				return damage / Time * (t / 1000);
+			}
+				
+			return damage / 10 * (t / 1000);
+		}
+
+		// Spell
+
+		public bool SpeedofLight ()
+		{
+			return CastSelf ("Speed of Light", () => Usable ("Speed of Light"));
+		}
+
+		public bool BloodFury (UnitObject u = null)
+		{
+			u = u ?? Target;
+			return CastSelf ("Blood Fury", () => Usable ("Blood Fury") && u.IsInCombatRangeAndLoS && (IsElite (u) || IsPlayer (u) || EnemyInRange (10) > 2));
+		}
+
+		public bool Berserking (UnitObject u = null)
+		{
+			u = u ?? Target;
+			return CastSelf ("Berserking", () => Usable ("Berserking") && u.IsInCombatRangeAndLoS && (IsElite (u) || IsPlayer (u) || EnemyInRange (10) > 2));
+		}
+
+		public bool ArcaneTorrent (UnitObject u = null)
+		{
+			u = u ?? Target;
+			return CastSelf ("Arcane Torrent", () => Usable ("Arcane Torrent") && u.IsInCombatRangeAndLoS && (IsElite (u) || IsPlayer (u) || EnemyInRange (10) > 2));
+		}
+
+		public bool HolyAvenger (UnitObject u = null)
+		{
+			u = u ?? Target;
+			return CastSelf ("Holy Avenger", () => Usable ("Holy Avenger") && u.IsInCombatRangeAndLoS && (IsElite (u) || IsPlayer (u) || EnemyInRange (10) > 2));
+		}
+
+		public bool Seraphim (UnitObject u = null)
+		{
+			u = u ?? Target;
+			return CastSelf ("Seraphim", () => Usable ("Seraphim") && u.IsInCombatRangeAndLoS);
+		}
+
+		public bool DivineProtection (UnitObject u = null)
+		{
+			u = u ?? Target;
+			return CastSelf ("Divine Protection", () => Usable ("Divine Protection") && Health (Me) < 0.8);
+		}
+
+		public bool GuardianofAncientKings (UnitObject u = null)
+		{
+			u = u ?? Target;
+			return Cast ("Guardian of Ancient Kings", () => Usable ("Guardian of Ancient Kings") && Health (Me) < 0.4 && u.IsInLoS && Range (u) <= 30, u);
+		}
+
+		public bool ArdentDefender (UnitObject u = null)
+		{
+			u = u ?? Target;
+			return CastSelf ("Ardent Defender", () => Usable ("Ardent Defender") && Health (Me) < 0.2);
+		}
+
+		public bool EternalFlame (UnitObject u = null)
+		{
+			u = u ?? Target;
+			return Cast ("Eternal Flame", () => Usable ("Eternal Flame") && HolyPower >= 1 && u.IsInLoS && Range (u) <= 40, u);
+		}
+
+		public bool ShieldoftheRighteous (UnitObject u = null)
+		{
+			u = u ?? Target;
+			return Cast ("Shield of the Righteous", () => Usable ("Shield of the Righteous") && HolyPower >= 3 && u.IsInLoS && Range (u) <= 5, u);
+		}
+	}
+}
+
--- Paladin/SerbPaladinProtiction.cs
+++ Paladin/SerbPaladinProtiction.cs
@@ -1,75 +1,124 @@
 ﻿using System;
+using ReBot.API;
 
 namespace ReBot
 {
-	public class SerbPaladinProtiction
+	[Rotation ("Serb Paladin Protiction SC", "Serb", WoWClass.Paladin, Specialization.PaladinProtection, 5, 25)]
+
+	public class SerbPaladinProtictionSC : SerbPaladin
 	{
-		public SerbPaladinProtiction ()
+		public SerbPaladinProtictionSC ()
 		{
+		}
+
+		public override bool OutOfCombat ()
+		{
+			//	actions.precombat=flask,type=greater_draenic_stamina_flask
+			//	actions.precombat+=/flask,type=greater_draenic_strength_flask,if=role.attack|using_apl.max_dps
+			//	actions.precombat+=/food,type=whiptail_fillet
+			//	actions.precombat+=/food,type=pickled_eel,if=role.attack|using_apl.max_dps
+			//	actions.precombat+=/blessing_of_kings,if=(!aura.str_agi_int.up)&(aura.mastery.up)
+			//	actions.precombat+=/blessing_of_might,if=!aura.mastery.up
+			//	actions.precombat+=/seal_of_insight
+			//	actions.precombat+=/seal_of_righteousness,if=role.attack|using_apl.max_dps
+			//	actions.precombat+=/sacred_shield
+			//	# Snapshot raid buffed stats before combat begins and pre-potting is done.
+			//	actions.precombat+=/snapshot_stats
+			//	actions.precombat+=/potion,name=draenic_armor
+			if (InCombat) {
+				InCombat = false;
+				return true;
+			}
+
+			return false;
+		}
+
+		public override void Combat ()
+		{
+			if (!InCombat) {
+				InCombat = true;
+				StartBattle = DateTime.Now;
+			}
+
+			//	actions=auto_attack
+			//	actions+=/speed_of_light,if=movement.remains>1
+			if (Range () > 15)
+				SpeedofLight ();
+			//	actions+=/blood_fury
+			BloodFury ();
+			//	actions+=/berserking
+			Berserking ();
+			//	actions+=/arcane_torrent
+			ArcaneTorrent ();
+			//	# Off-GCD spells.
+
+			//	actions+=/holy_avenger
+			HolyAvenger ();
+			//	actions+=/potion,name=draenic_armor,if=buff.shield_of_the_righteous.down&buff.seraphim.down&buff.divine_protection.down&buff.guardian_of_ancient_kings.down&buff.ardent_defender.down
+			//	actions+=/seraphim
+			Seraphim ();
+			//	actions+=/divine_protection,if=time<5|!talent.seraphim.enabled|(buff.seraphim.down&cooldown.seraphim.remains>5&cooldown.seraphim.remains<9)
+			if (Time < 5 || !HasSpell ("Seraphim") || (!Me.HasAura ("Seraphim") && Cooldown ("Seraphim") > 5 && Cooldown ("Seraphim") < 9))
+				DivineProtection ();
+			//	actions+=/guardian_of_ancient_kings,if=time<5|(buff.holy_avenger.down&buff.shield_of_the_righteous.down&buff.divine_protection.down)
+			if (Time < 5 || (!Me.HasAura ("Holy Avenger") && !Me.HasAura ("Shield of the Righteous") && !Me.HasAura ("Divine Protection")))
+				GuardianofAncientKings ();
+			//	actions+=/ardent_defender,if=time<5|(buff.holy_avenger.down&buff.shield_of_the_righteous.down&buff.divine_protection.down&buff.guardian_of_ancient_kings.down)
+			if (Time < 5 || (!Me.HasAura ("Holy Avenger") && !Me.HasAura ("Shield of the Righteous") && !Me.HasAura ("Divine Protection") && !Me.HasAura ("Guardian of Ancient Kings")))
+				ArdentDefender ();
+			//	actions+=/eternal_flame,if=buff.eternal_flame.remains<2&buff.bastion_of_glory.react>2&(holy_power>=3|buff.divine_purpose.react|buff.bastion_of_power.react)
+			if (Me.AuraTimeRemaining ("Eternal Flame") < 2 && Me.GetAura ("Bastion of Glory").StackCount > 2 && (HolyPower >= 3 || Me.HasAura ("Divine Purpose") || Me.HasAura ("Bastion of Power"))) {
+				if (EternalFlame ())
+					return;
+			}
+			//	actions+=/eternal_flame,if=buff.bastion_of_power.react&buff.bastion_of_glory.react>=5
+			if (Me.HasAura ("Bastion of Power") && Me.GetAura ("Bastion of Glory").StackCount >= 5) {
+				if (EternalFlame ())
+					return;
+			}
+			//	actions+=/shield_of_the_righteous,if=buff.divine_purpose.react
+			if (Me.HasAura ("Divine Purpose"))
+				ShieldoftheRighteous ();
+			//	actions+=/shield_of_the_righteous,if=(holy_power>=5|incoming_damage_1500ms>=health.max*0.3)&(!talent.seraphim.enabled|cooldown.seraphim.remains>5)
+			if (HolyPower >= 5 || DamageTaken(1500) >= Me.MaxHealth * 0.3)&(!talent.seraphim.enabled|cooldown.seraphim.remains>5))
+				ShieldoftheRighteous ();
+			//	actions+=/shield_of_the_righteous,if=buff.holy_avenger.remains>time_to_hpg&(!talent.seraphim.enabled|cooldown.seraphim.remains>time_to_hpg)
+
+			//	# GCD-bound spells
+			if (HasGlobalCooldown () && Gcd)
+				return;
+			//	actions+=/seal_of_insight,if=talent.empowered_seals.enabled&!seal.insight&buff.uthers_insight.remains<cooldown.judgment.remains
+			//	actions+=/seal_of_righteousness,if=talent.empowered_seals.enabled&!seal.righteousness&buff.uthers_insight.remains>cooldown.judgment.remains&buff.liadrins_righteousness.down
+			//	actions+=/avengers_shield,if=buff.grand_crusader.react&active_enemies>1&!glyph.focused_shield.enabled
+			//	actions+=/hammer_of_the_righteous,if=active_enemies>=3
+			//	actions+=/crusader_strike
+			//	actions+=/wait,sec=cooldown.crusader_strike.remains,if=cooldown.crusader_strike.remains>0&cooldown.crusader_strike.remains<=0.35
+			//	actions+=/judgment,cycle_targets=1,if=glyph.double_jeopardy.enabled&last_judgment_target!=target
+			//	actions+=/judgment
+			//	actions+=/wait,sec=cooldown.judgment.remains,if=cooldown.judgment.remains>0&cooldown.judgment.remains<=0.35
+			//	actions+=/avengers_shield,if=active_enemies>1&!glyph.focused_shield.enabled
+			//	actions+=/holy_wrath,if=talent.sanctified_wrath.enabled
+			//	actions+=/avengers_shield,if=buff.grand_crusader.react
+			//	actions+=/sacred_shield,if=target.dot.sacred_shield.remains<2
+			//	actions+=/holy_wrath,if=glyph.final_wrath.enabled&target.health.pct<=20
+			//	actions+=/avengers_shield
+			//	actions+=/lights_hammer,if=!talent.seraphim.enabled|buff.seraphim.remains>10|cooldown.seraphim.remains<6
+			//	actions+=/holy_prism,if=!talent.seraphim.enabled|buff.seraphim.up|cooldown.seraphim.remains>5|time<5
+			//	actions+=/consecration,if=target.debuff.flying.down&active_enemies>=3
+			//	actions+=/execution_sentence,if=!talent.seraphim.enabled|buff.seraphim.up|time<12
+			//	actions+=/hammer_of_wrath
+			//	actions+=/sacred_shield,if=target.dot.sacred_shield.remains<8
+			//	actions+=/consecration,if=target.debuff.flying.down
+			//	actions+=/holy_wrath
+			//	actions+=/seal_of_insight,if=talent.empowered_seals.enabled&!seal.insight&buff.uthers_insight.remains<=buff.liadrins_righteousness.remains
+			//	actions+=/seal_of_righteousness,if=talent.empowered_seals.enabled&!seal.righteousness&buff.liadrins_righteousness.remains<=buff.uthers_insight.remains
+			//	actions+=/sacred_shield
+			//	actions+=/flash_of_light,if=talent.selfless_healer.enabled&buff.selfless_healer.stack>=3
 		}
 	}
 }
 
-//	actions.precombat=flask,type=greater_draenic_stamina_flask
-//	actions.precombat+=/flask,type=greater_draenic_strength_flask,if=role.attack|using_apl.max_dps
-//	actions.precombat+=/food,type=whiptail_fillet
-//	actions.precombat+=/food,type=pickled_eel,if=role.attack|using_apl.max_dps
-//	actions.precombat+=/blessing_of_kings,if=(!aura.str_agi_int.up)&(aura.mastery.up)
-//	actions.precombat+=/blessing_of_might,if=!aura.mastery.up
-//	actions.precombat+=/seal_of_insight
-//	actions.precombat+=/seal_of_righteousness,if=role.attack|using_apl.max_dps
-//	actions.precombat+=/sacred_shield
-//	# Snapshot raid buffed stats before combat begins and pre-potting is done.
-//	actions.precombat+=/snapshot_stats
-//	actions.precombat+=/potion,name=draenic_armor
-//
-//	# Executed every time the actor is available.
-//
-//	actions=auto_attack
-//	actions+=/speed_of_light,if=movement.remains>1
-//	actions+=/blood_fury
-//	actions+=/berserking
-//	actions+=/arcane_torrent
-//	# Off-GCD spells.
-//	actions+=/holy_avenger
-//	actions+=/potion,name=draenic_armor,if=buff.shield_of_the_righteous.down&buff.seraphim.down&buff.divine_protection.down&buff.guardian_of_ancient_kings.down&buff.ardent_defender.down
-//	actions+=/seraphim
-//	actions+=/divine_protection,if=time<5|!talent.seraphim.enabled|(buff.seraphim.down&cooldown.seraphim.remains>5&cooldown.seraphim.remains<9)
-//	actions+=/guardian_of_ancient_kings,if=time<5|(buff.holy_avenger.down&buff.shield_of_the_righteous.down&buff.divine_protection.down)
-//	actions+=/ardent_defender,if=time<5|(buff.holy_avenger.down&buff.shield_of_the_righteous.down&buff.divine_protection.down&buff.guardian_of_ancient_kings.down)
-//	actions+=/eternal_flame,if=buff.eternal_flame.remains<2&buff.bastion_of_glory.react>2&(holy_power>=3|buff.divine_purpose.react|buff.bastion_of_power.react)
-//	actions+=/eternal_flame,if=buff.bastion_of_power.react&buff.bastion_of_glory.react>=5
-//	actions+=/shield_of_the_righteous,if=buff.divine_purpose.react
-//	actions+=/shield_of_the_righteous,if=(holy_power>=5|incoming_damage_1500ms>=health.max*0.3)&(!talent.seraphim.enabled|cooldown.seraphim.remains>5)
-//	actions+=/shield_of_the_righteous,if=buff.holy_avenger.remains>time_to_hpg&(!talent.seraphim.enabled|cooldown.seraphim.remains>time_to_hpg)
-//	# GCD-bound spells
-//	actions+=/seal_of_insight,if=talent.empowered_seals.enabled&!seal.insight&buff.uthers_insight.remains<cooldown.judgment.remains
-//	actions+=/seal_of_righteousness,if=talent.empowered_seals.enabled&!seal.righteousness&buff.uthers_insight.remains>cooldown.judgment.remains&buff.liadrins_righteousness.down
-//	actions+=/avengers_shield,if=buff.grand_crusader.react&active_enemies>1&!glyph.focused_shield.enabled
-//	actions+=/hammer_of_the_righteous,if=active_enemies>=3
-//	actions+=/crusader_strike
-//	actions+=/wait,sec=cooldown.crusader_strike.remains,if=cooldown.crusader_strike.remains>0&cooldown.crusader_strike.remains<=0.35
-//	actions+=/judgment,cycle_targets=1,if=glyph.double_jeopardy.enabled&last_judgment_target!=target
-//	actions+=/judgment
-//	actions+=/wait,sec=cooldown.judgment.remains,if=cooldown.judgment.remains>0&cooldown.judgment.remains<=0.35
-//	actions+=/avengers_shield,if=active_enemies>1&!glyph.focused_shield.enabled
-//	actions+=/holy_wrath,if=talent.sanctified_wrath.enabled
-//	actions+=/avengers_shield,if=buff.grand_crusader.react
-//	actions+=/sacred_shield,if=target.dot.sacred_shield.remains<2
-//	actions+=/holy_wrath,if=glyph.final_wrath.enabled&target.health.pct<=20
-//	actions+=/avengers_shield
-//	actions+=/lights_hammer,if=!talent.seraphim.enabled|buff.seraphim.remains>10|cooldown.seraphim.remains<6
-//	actions+=/holy_prism,if=!talent.seraphim.enabled|buff.seraphim.up|cooldown.seraphim.remains>5|time<5
-//	actions+=/consecration,if=target.debuff.flying.down&active_enemies>=3
-//	actions+=/execution_sentence,if=!talent.seraphim.enabled|buff.seraphim.up|time<12
-//	actions+=/hammer_of_wrath
-//	actions+=/sacred_shield,if=target.dot.sacred_shield.remains<8
-//	actions+=/consecration,if=target.debuff.flying.down
-//	actions+=/holy_wrath
-//	actions+=/seal_of_insight,if=talent.empowered_seals.enabled&!seal.insight&buff.uthers_insight.remains<=buff.liadrins_righteousness.remains
-//	actions+=/seal_of_righteousness,if=talent.empowered_seals.enabled&!seal.righteousness&buff.liadrins_righteousness.remains<=buff.uthers_insight.remains
-//	actions+=/sacred_shield
-//	actions+=/flash_of_light,if=talent.selfless_healer.enabled&buff.selfless_healer.stack>=3
+
 //
 //	# This is a high-DPS (but low-survivability) configuration.
 //	# Invoke by adding "actions+=/run_action_list,name=max_dps" to the beginning of the default APL.
