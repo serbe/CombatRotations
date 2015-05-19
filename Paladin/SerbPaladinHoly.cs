a7bcd0dda52f7149a0fdc2cf5b1df08f769934c7
serbe <mkoctuk@gmail.com>
2015-05-19 22:34:48 +0300

    add holy pal

---
+++ Paladin/SerbPaladinHoly.cs
@@ -0,0 +1,30 @@
+﻿using ReBot.API;
+using Newtonsoft.Json;
+
+namespace ReBot
+{
+	[Rotation ("SC Paladin Protection", "Serb", WoWClass.Paladin, Specialization.PaladinHoly, 40, 25)]
+
+	public class SerbPaladinHolySC : SerbPaladin
+	{
+		[JsonProperty ("Use GCD")]
+		public bool Gcd = true;
+
+		public SerbPaladinHolySC ()
+		{
+			GroupBuffs = new[] { "Blessing of Kings" };
+			PullSpells = new[] { "Judgment" };
+		}
+
+		public override bool OutOfCombat ()
+		{
+			return false;
+		}
+
+		public override void Combat ()
+		{
+
+		}
+	}
+}
+
